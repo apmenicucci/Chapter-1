69932848f9e8d8166c59c151375505fed975d350
apmenicucci <adam.menicucci@salesianum.org>
2023-09-15 14:47:53 -0400

    in class

---
+++ ShoesSocks.java
@@ -0,0 +1,8 @@
+public class ShoesSocks {
+    public double rev(int shoesSold, int socksSold) {
+        int priceOfShoes = 70;
+        double priceOfSocks = 2.50;
+
+        return(shoesSold*priceOfShoes + socksSold*priceOfSocks);
+    }
+}
